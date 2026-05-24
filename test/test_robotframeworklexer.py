cb74f6a7823f9ef28e0690188c6de2f51cc74911
HelioGuilherme66 <helioxentric@gmail.com>
2026-05-24 18:14:08 +0100

    Update tests

--- test/test_robotframeworklexer.py
+++ test/test_robotframeworklexer.py
@@ -58,8 +58,7 @@ class TestVariableTokenizer(unittest.TestCase):
                      ('${', SYNTAX), ('var', VARIABLE), ('${', SYNTAX),
                      ('inside', VARIABLE), ('}', SYNTAX), ('}', SYNTAX))
         self._verify('@{%{var${not}} end',
-                     ('@{', SYNTAX), ('%{', SYNTAX), ('var', VARIABLE),
-                     ('${', SYNTAX), ('not', VARIABLE),
+                     ('@{', SYNTAX), ('%{', SYNTAX), ('var${not', VARIABLE),
                      ('}', SYNTAX), ('}', SYNTAX), (' end', ARGUMENT))
 
     def test_var_item(self):
